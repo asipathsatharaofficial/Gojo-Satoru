190a9f431f7fbd0eb52ceb0ecf1fcb1ca8912318
Asipathsathara official <109749146+asipathsatharaofficial@users.noreply.github.com>
2022-07-22 00:31:17 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,16 +19,16 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
+global.owner = ['+94702812230']
+global.premium = ['+94702812230']
+global.ownernomer = '+94702812230'
+global.ownername = '🐦K Max Kalhara'
+global.botname = 'Pandora academy'
 global.footer = '©NexusNwInc.'
 global.ig = 'https://github.com/nexusNw'
 global.region = 'India, South India, Kerala'
 global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
-global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
+global.myweb = 'https://youtube.com/channel/UCBp_2BMODEdWhhLNnhKuduw'
 global.packname = 'Gojo-Satoru'
 global.author = '🐦Star Boy Nexus'
 global.sessionName = 'session'
